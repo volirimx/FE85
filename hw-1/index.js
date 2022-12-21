9aa761bbbc5048d2c4f08d16ea8f74844957b058
cheshochek <anna_cheshok@mail.ru>
2022-12-21 16:06:04 +0300

    edit hw-1

--- hw-1/index.js
+++ hw-1/index.js
@@ -18,22 +18,22 @@ const subjects = {
 }
 
 // Exercise-1
-function showNamesOfSubjects(obj) {
+function showNamesOfSubjects (obj) {
   return Object.keys(obj).join(',')
 }
 
 // Exercise-2
-function countOfStudentsAndTeachers(obj) {
+function countOfStudentsAndTeachers (obj) {
   return Object.values(obj).reduce((acc, current) => (acc += current.students + current.teachers), 0)
 }
 
 // Exercise-3
-function countAverageOfStudents(obj) {
+function countAverageOfStudents (obj) {
   return Object.values(obj).reduce((acc, current) => (acc += current.students), 0) / Object.keys(obj).length
 }
 
 // Exercise-4, Exercise-5
-function sortSubjectsByTeachers(obj) {
-  const arrayOfSubjects = [...Object.values(obj)]
+function sortSubjectsByTeachers (obj) {
+  const arrayOfSubjects = Object.values(obj)
   return arrayOfSubjects.sort((a, b) => b.teachers - a.teachers)
 }
