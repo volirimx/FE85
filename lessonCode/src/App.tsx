c8f90079898b33b8efa1bace0e8bd625fa5a45a2
volirimx <denis.work2000@gmail.com>
2023-02-08 19:19:44 +0300

    a

--- lessonCode/src/App.tsx
+++ lessonCode/src/App.tsx
@@ -7,40 +7,17 @@ import { ThemeContext, useInitThemeContext } from "./context/theme";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-interface ICharacter {
-  created: string;
-  episode: string[];
-  gender: string;
-  id: number;
-  image: string;
-  location: {
-    name: string;
-    url: string;
-  };
-  name: string;
-  origin: {
-    name: string;
-    url: string;
-  };
-  species: string;
-  status: string;
-  type: string;
-  url: string;
-}
-
 function App() {
   const themeContextValues = useInitThemeContext();
 
   return (
     <Provider store={store}>
-      <ThemeContext.Provider value={themeContextValues}>
-        <div className={"light" ? "AppLight" : "App"}>
-          <Header />
-          {/* <Login /> */}
-          <Cards />
-          <Footer />
-        </div>
-      </ThemeContext.Provider>
+      <div className={"light" ? "AppLight" : "App"}>
+        <Header />
+        {/* <Login /> */}
+        <Cards />
+        <Footer />
+      </div>
     </Provider>
   );
 }
